c47b50cb41aa40bbbd869fd050b20f281fa33dfd
ebrahim-sameh <ebrahimsameh16@gmail.com>
2026-08-12 14:55:08 +0300

    perf: preload bootstrap-icons font to reduce icon CLS

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,17 @@ import { HashRouter } from 'react-router-dom'
 import './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import bootstrapIconsWoff2 from 'bootstrap-icons/font/fonts/bootstrap-icons.woff2?url'
 import './index.css'
 import App from './App.jsx'
+
+const iconPreload = document.createElement('link')
+iconPreload.rel = 'preload'
+iconPreload.as = 'font'
+iconPreload.type = 'font/woff2'
+iconPreload.crossOrigin = 'anonymous'
+iconPreload.href = bootstrapIconsWoff2
+document.head.appendChild(iconPreload)
 
 // HashRouter avoids GitHub Pages document 404s (and console errors) on deep links.
 createRoot(document.getElementById('root')).render(
