05d6041d08e817beb54aa8a4fb09fc6accb58afd
ebrahim-sameh <ebrahimsameh16@gmail.com>
2026-08-12 03:10:52 +0300

    feat: add departments programs faculty events announcements data

---
+++ src/data/events.js
@@ -0,0 +1,24 @@
+const events = [
+  {
+    key: 'orientationWeek',
+    type: 'academic',
+    date: '2026-09-20',
+  },
+  {
+    key: 'icpcTraining',
+    type: 'contest',
+    date: '2026-10-05',
+  },
+  {
+    key: 'careerDay',
+    type: 'career',
+    date: '2026-11-12',
+  },
+  {
+    key: 'researchSeminar',
+    type: 'academic',
+    date: '2026-12-02',
+  },
+];
+
+export default events;
---
+++ src/data/faculty.js
@@ -0,0 +1,34 @@
+const faculty = [
+  {
+    key: 'dean',
+    department: 'intelligentSystems',
+    role: 'dean',
+  },
+  {
+    key: 'amrAmin',
+    department: 'intelligentSystems',
+    role: 'professor',
+  },
+  {
+    key: 'reemHassan',
+    department: 'businessAnalytics',
+    role: 'associate',
+  },
+  {
+    key: 'saraMahmoud',
+    department: 'mediaAnalytics',
+    role: 'lecturer',
+  },
+  {
+    key: 'omarFarouk',
+    department: 'cyberSecurity',
+    role: 'associate',
+  },
+  {
+    key: 'nadaYoussef',
+    department: 'healthInformatics',
+    role: 'lecturer',
+  },
+];
+
+export default faculty;
